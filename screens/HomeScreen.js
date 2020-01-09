30e0204fcf9afcfd918aab25c9d6b3ef258bfd2d
Florian <florian.guillet@ecole-isitech.fr>
2020-01-09 12:19:29 +0100

    NotificationToken

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,7 @@
 import React , { Component } from 'react';
 import { Container, List, ListItem, Content, Text, Button, Icon, Header } from 'native-base';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
 
 export default class HomeScreen extends Component {
   constructor(props) {
@@ -9,6 +11,50 @@ export default class HomeScreen extends Component {
   static navigationOptions =  {
     title: 'Home',
   };
+
+  async componentDidMount() {
+
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    );
+    let finalStatus = existingStatus;
+  
+    // only ask if permissions have not already been determined, because
+    // iOS won't necessarily prompt the user a second time.
+    if (existingStatus !== 'granted') {
+      // Android remote notification permissions are granted during the app
+      // install, so this will only ask on iOS
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+  
+    // Stop here if the user did not grant permissions
+    if (finalStatus !== 'granted') {
+      return;
+    }
+  
+    // Get the token that uniquely identifies this device
+    let token = await Notifications.getExpoPushTokenAsync();
+    console.log(token)
+  
+    // POST the token to your backend server from where you can retrieve it to send push notifications.
+    /*return fetch(PUSH_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        token: {
+          value: token,
+        },
+        user: {
+          username: 'Brent',
+        },
+      }),
+    });*/
+
+  }
 
   addCharacter() {}
 
